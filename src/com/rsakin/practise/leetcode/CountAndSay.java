a91a76398ab2bac362d2802f1056eba8f32aff27
ramazansakin <ramazansakin63@gmail.com>
2023-01-09 18:25:04 +0300

    Solution for AddTwoLL & CouÄbtAndSay - leetcode

---
+++ src/com/rsakin/practise/leetcode/CountAndSay.java
@@ -0,0 +1,29 @@
+package com.rsakin.practise.leetcode;
+
+public class CountAndSay {
+
+    public String countAndSay(int n) {
+        String res = "1";
+        while (n-- > 1) {
+            StringBuilder tempRes = new StringBuilder();
+            for (int i = 0; i < res.length(); i++) {
+                int eqNumCounter = 1;
+                char numChar = res.charAt(i);
+                while (i + 1 < res.length() && numChar == res.charAt(i + 1)) {
+                    ++i;
+                    ++eqNumCounter;
+                }
+                tempRes.append(eqNumCounter).append(numChar);
+            }
+            res = tempRes.toString();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int num = 4;
+        System.out.println("Solution for " + num + " : " + new CountAndSay().countAndSay(num));
+
+    }
+}
