cfe6c39abeef8c65e67e8bcb9367111e147991e6
ramazansakin <ramazansakin63@gmail.com>
2023-01-10 09:28:14 +0300

    BFS impl in Java - iterative

---
+++ src/com/rsakin/practise/Algo/BFSGraph.java
@@ -0,0 +1,60 @@
+package com.rsakin.practise.Algo;
+
+// BFS algorithm in Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFSGraph {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    // Create a graph
+    BFSGraph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    // Add edges to the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    // BFS algorithm
+    void bfs(int vertex) {
+        boolean visited[] = new boolean[V];
+        Queue<Integer> queue = new LinkedList();
+
+        visited[vertex] = true;
+        queue.add(vertex);
+
+        while (!queue.isEmpty()) {
+            vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            for (int n : adj[vertex]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        BFSGraph g = new BFSGraph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal " + "(starting from vertex 2)");
+
+        g.bfs(2);
+    }
+}
