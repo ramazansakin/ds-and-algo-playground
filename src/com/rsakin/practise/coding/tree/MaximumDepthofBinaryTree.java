1262f969e60c973736c995c8cf852ae8e7d09837
ramazansakin <ramazansakin63@gmail.com>
2024-03-22 08:11:04 +0300

    feat : max depth of a tree sol added - leetcode

---
+++ src/com/rsakin/practise/coding/tree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,35 @@
+package com.rsakin.practise.coding.tree;
+
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class MaximumDepthofBinaryTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+
+}
