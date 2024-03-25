85b11cd9a5a4afd9426d69657c66299608c63516
ramazansakin <ramazansakin63@gmail.com>
2024-03-25 07:39:32 +0300

    feat : threeSum sol added - leetcode

---
+++ src/com/rsakin/practise/leetcode/ThreeSumSolution.java
@@ -0,0 +1,43 @@
+package com.rsakin.practise.leetcode;
+
+import java.util.*;
+
+public class ThreeSumSolution {
+
+    // 1.way - naive solution which is brute-force approach
+    // Take 3 nested for loops and check all possible triplets which their sum is 0.
+    // Time: O(n^3) , Space: O(1) - no need extra space apart from some temporary variables.
+
+
+    // 2.way - better efficient
+    // First, we need to sort the list -> O(n.log(n))
+    // Then, we can loop each index and choose 2nd and 3rd index by "Two Pointer" approach (List is SORTED!)
+    // So, it ll be one outher for loop and one inner while loop
+    // Time: O(n^2) , Space: O(1)
+
+
+    // 3.way - better efficient with extra space
+    // No need sorting, just we can use "Map" structure to hold each complements
+    // Like, TwoSum problem, we ll also loop for the first variable
+    // 2nd and 3rd variables ll be chosen on "Complementary Map"
+    // It provides better efficiency because NO NEED to sort list
+    // Time: O(n^2) , Space: O(n) - Map usage
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
+        for (int first = 0; first < nums.length - 2; first++) {
+            int left = first + 1;
+            int right = nums.length - 1;
+            // two pointer approach
+            while (left < right) {
+                int sum = nums[first] + nums[left] + nums[right];
+                if (sum == 0)
+                    res.add(Arrays.asList(nums[first], nums[left++], nums[right--]));
+                else if (sum < 0) left++;
+                else right--;
+            }
+        }
+        return new ArrayList<>(res);
+    }
+
+}
