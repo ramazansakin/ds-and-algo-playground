0efa42a754d91c5e42c9ee064d15d64891934ae2
ramazansakin <ramazansakin63@gmail.com>
2024-03-26 04:36:37 +0300

    feat : twosum sol up - leetcode

--- src/com/rsakin/practise/leetcode/TwoSum.java
+++ src/com/rsakin/practise/leetcode/TwoSum.java
@@ -6,6 +6,15 @@ import java.util.Map;
 
 public class TwoSum {
 
+    // numbers = [2,7,11,15], target = 9
+    // response :  [1,2]
+    // 1.way is the naive solution is brute force approach
+    // we can try all the number couples with 2 for loop
+    // Time: O(n^2) , Space: O(1)
+
+    // 2.way - better efficient but it contains extra space and this solution for "UNORDERED LIST"
+    // This contains extra space like O(n) also this can be use if given list is un-ordered
+    // But given list is ordered, we can use "Two Pointer Approach" to find the target
     public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
@@ -20,6 +29,30 @@ public class TwoSum {
         }
         return result;
     }
+
+
+    // 3.way - better efficient and it DOES NOT contain extra space and this solution for "ORDERED LIST"
+    // If the list is not ordered, then we can order the list first then use this method as well!
+    // Arrays.sort(numbers);
+    public int[] twoSum2(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1}; // 1-indexed array
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        // No solution found
+        return new int[]{-1, -1};
+    }
+
 
     public static void main(String[] args) {
 
