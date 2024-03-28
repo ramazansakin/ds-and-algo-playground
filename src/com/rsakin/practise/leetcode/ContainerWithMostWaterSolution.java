bd69bb32203e379f3bca455c0346e835712f282b
ramazansakin <ramazansakin63@gmail.com>
2024-03-28 10:33:27 +0300

    feat : ContainerWithMostWaterSolution sol up - leetcode

--- src/com/rsakin/practise/leetcode/ContainerWithMostWaterSolution.java
+++ src/com/rsakin/practise/leetcode/ContainerWithMostWaterSolution.java
@@ -2,6 +2,15 @@ package com.rsakin.practise.leetcode;
 
 public class ContainerWithMostWaterSolution {
 
+    // 1.way which is naive solution with brute-force approach
+    // 2 for loop to compare all possible combinations
+    // T: O(n^2)  --  S: O(1)
+
+
+    // 2.way is two pointer approach
+    // start and end indexes and comparing each index one by one between start and end
+    // then calculate and check the area until start < end
+    // T: O(n)  --  S: O(1)
     public int maxArea(int[] height) {
         int startInd = 0, endInd = height.length - 1, maxArea = 0;
         while (startInd < endInd) {
