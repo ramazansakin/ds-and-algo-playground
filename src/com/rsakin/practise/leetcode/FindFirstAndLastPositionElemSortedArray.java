bb30609fd3fa23062be9de9dff6a3e0a388d124d
ramazansakin <ramazansakin63@gmail.com>
2024-03-28 11:41:06 +0300

    feat : FindFirstAndLastPositionElemSortedArray sol up - leetcode

--- src/com/rsakin/practise/leetcode/FindFirstAndLastPositionElemSortedArray.java
+++ src/com/rsakin/practise/leetcode/FindFirstAndLastPositionElemSortedArray.java
@@ -4,31 +4,49 @@ import java.util.Arrays;
 
 public class FindFirstAndLastPositionElemSortedArray {
 
-    public int[] searchRange(int[] A, int target) {
-        // find the target if there is defined in array, if not so, return -1, -1
-        int start = firstGreaterOrEqual(A, target);
-        if (start == A.length || A[start] != target) {
-            return new int[]{-1, -1};
-        }
-        // find the last occurrence of the target via comparing next greater element
-        return new int[]{start, firstGreaterOrEqual(A, target + 1) - 1};
-    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
 
-    private int firstGreaterOrEqual(int[] A, int target) {
-        int low = 0, high = A.length;
-        while (low < high) {
-            int mid = (high / 2) + (low / 2); // prevent overflow
-            if (A[mid] < target) low = mid + 1;
-            else high = mid;
+        // Find the starting position
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        return low;
+        if (left < nums.length && nums[left] == target) {
+            result[0] = left;
+        } else {
+            return result; // Target not found
+        }
+
+        // Find the ending position
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        result[1] = right;
+
+        return result;
     }
 
     public static void main(String[] args) {
 
-        int[] nums = {5, 7, 7, 8, 8};
+        int[] nums = {5, 7, 7, 8, 8, 10};
 
-        System.out.println("Solution : " + Arrays.toString(new FindFirstAndLastPositionElemSortedArray().searchRange(nums, 8)));
+        System.out.println("Solution for 8 : " + Arrays.toString(new FindFirstAndLastPositionElemSortedArray().searchRange(nums, 8)));
+
+        System.out.println();
+
+        System.out.println("Solution for 5 : " + Arrays.toString(new FindFirstAndLastPositionElemSortedArray().searchRange(nums, 5)));
 
     }
 
