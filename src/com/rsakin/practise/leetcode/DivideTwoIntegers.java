a6a9d00a244de78794f611a5637d7f78d0f39de9
ramazansakin <ramazansakin63@gmail.com>
2024-03-28 11:11:17 +0300

    feat : DivideTwoIntegers sol up with alternative

--- src/com/rsakin/practise/leetcode/DivideTwoIntegers.java
+++ src/com/rsakin/practise/leetcode/DivideTwoIntegers.java
@@ -3,7 +3,8 @@ package com.rsakin.practise.leetcode;
 public class DivideTwoIntegers {
 
     public static int divide(int dividend, int divisor) {
-        if (dividend == 1 << 31 && divisor == -1) return (1 << 31) - 1;
+        if (dividend == 1 << 31 && divisor == -1) return (1 << 31) - 1; // edge-case
+
         int tempDividend = Math.abs(dividend), tempDivisor = Math.abs(divisor), res = 0, x;
         while (tempDividend - tempDivisor >= 0) {
             for (x = 0; tempDividend - (tempDivisor << x << 1) >= 0; ++x) ;
@@ -15,8 +16,35 @@ public class DivideTwoIntegers {
 
     public static void main(String[] args) {
 
-        System.out.println("Result : " + divide(-2147483648, -1));
+        System.out.println("Result : " + divide(-23234, 56));
+        System.out.println("Result2 : " + divide2(-23234, 56));
+        System.out.println();
 
+        System.out.println("Result : " + divide(52344234, 343));
+        System.out.println("Result2 : " + divide2(52344234, 343));
+        System.out.println();
+
+        // edge-case check
+        System.out.println("Result : " + divide(-2147483648, -1));
+        System.out.println("Result2 : " + divide2(-2147483648, -1));
+
+    }
+
+
+    // Without using shifting
+    public static int divide2(int dividend, int divisor) {
+        if (dividend == 1 << 31 && divisor == -1) return (1 << 31) - 1; // edge-case
+
+        int tempDividend = Math.abs(dividend);
+        int tempDivisor = Math.abs(divisor);
+
+        int quotient = 0;
+        while (tempDividend >= tempDivisor) {
+            tempDividend -= tempDivisor;
+            quotient++;
+        }
+
+        return (dividend > 0) == (divisor > 0) ? quotient : -quotient;  // check the sign
     }
 
 }
