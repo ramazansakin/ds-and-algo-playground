fe600bdc5271d31a32727dbbee8e37580671b60c
ramazansakin <ramazansakin63@gmail.com>
2024-03-30 10:10:44 +0300

    feat : GroupAnagramsSolution sol up - leetcode

--- src/com/rsakin/practise/leetcode/GroupAnagramsSolution.java
+++ src/com/rsakin/practise/leetcode/GroupAnagramsSolution.java
@@ -7,12 +7,13 @@ public class GroupAnagramsSolution {
     public static List<List<String>> groupAnagrams(String[] strs) {
 
         Map<String, List<String>> map = new HashMap<>();
-        for(String s : strs){
+        for (String s : strs) {
             char[] chars = s.toCharArray();
+            // sort all the words to group them depending on related char frequencies
             Arrays.sort(chars);
 
             String k = new String(chars);
-            if(!map.containsKey(k)){
+            if (!map.containsKey(k)) {
                 map.put(k, new ArrayList<>());
             }
             map.get(k).add(s);
@@ -21,14 +22,17 @@ public class GroupAnagramsSolution {
     }
 
     public static void main(String[] args) {
+        // test case - 1
         String[] strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
         System.out.println(groupAnagrams(strs1));
-        // Output: [["eat","tea","ate"],["tan","nat"],["bat"]]
+        // Output: [["eat","tea","ate"],["tan","nat"],["bat"]] -> order is not important
 
+        // test case - 2
         String[] strs2 = {""};
         System.out.println(groupAnagrams(strs2));
         // Output: [[""]]
 
+        // test case - 3
         String[] strs3 = {"a"};
         System.out.println(groupAnagrams(strs3));
         // Output: [["a"]]
