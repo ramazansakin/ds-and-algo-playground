547bb4e9517dc2b87215d965b355e2de49c7641a
ramazansakin <ramazansakin63@gmail.com>
2024-03-30 10:18:39 +0300

    feat : IsPalindromicNumber sol up - leetcode

--- src/com/rsakin/practise/leetcode/IsPalindromicNumber.java
+++ src/com/rsakin/practise/leetcode/IsPalindromicNumber.java
@@ -1,8 +1,9 @@
 package com.rsakin.practise.leetcode;
 
-public class IsPalindrome {
+public class IsPalindromicNumber {
 
     public boolean isPalindrome(int x) {
+
         if (x < 0) return false;
         int res = 0, tempX = x;
         while (tempX != 0) {
@@ -16,7 +17,10 @@ public class IsPalindrome {
     public static void main(String[] args) {
 
         int number = 1212;
-        System.out.println(number + " is palindromic number : " + new IsPalindrome().isPalindrome(number));
+        System.out.println(number + " is palindromic number : " + new IsPalindromicNumber().isPalindrome(number));
+
+        int number2 = 122221;
+        System.out.println(number2 + " is palindromic number : " + new IsPalindromicNumber().isPalindrome(number2));
 
     }
 
