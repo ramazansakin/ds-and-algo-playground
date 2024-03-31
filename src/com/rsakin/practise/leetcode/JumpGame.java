8300c649c4be172d48764c6c7d57810a5860fc49
ramazansakin <ramazansakin63@gmail.com>
2024-03-31 11:01:06 +0300

    feat : JumpGame sol up - leetcode

--- src/com/rsakin/practise/leetcode/JumpGame.java
+++ src/com/rsakin/practise/leetcode/JumpGame.java
@@ -21,7 +21,37 @@ public class JumpGame {
 
     public static void main(String[] args) {
 
+        // Test cases
+        int[] nums1 = {2, 3, 1, 1, 4};
+        System.out.println("Test Case 1");
+        System.out.println("Path : " + Arrays.toString(nums1));
+        System.out.println("Min step to end : " + new JumpGame().jump(nums1));
+
+        System.out.println("------------------------");
+
+        int[] nums2 = {2, 3, 0, 1, 4};
+        System.out.println("Test Case 2");
+        System.out.println("Path : " + Arrays.toString(nums2));
+        System.out.println("Min step to end : " + new JumpGame().jump(nums2));
+
+        System.out.println("------------------------");
+
+        int[] nums3 = {3, 2, 1, 0, 4};
+        System.out.println("Test Case 3");
+        System.out.println("Path : " + Arrays.toString(nums3));
+        System.out.println("Min step to end : " + new JumpGame().jump(nums3));
+
+        System.out.println("------------------------");
+
+        int[] nums4 = {1};
+        System.out.println("Test Case 4");
+        System.out.println("Path : " + Arrays.toString(nums4));
+        System.out.println("Min step to end : " + new JumpGame().jump(nums4));
+
+        System.out.println("------------------------");
+
         int[] path = {10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
+        System.out.println("Test Case 5");
         System.out.println("Path : " + Arrays.toString(path));
         System.out.println("Min step to end : " + new JumpGame().jump(path));
 
