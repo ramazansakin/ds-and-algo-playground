4b003834fe196eeba30d44cc373b3857567f5132
ramazansakin <ramazansakin63@gmail.com>
2024-03-31 10:55:16 +0300

    feat : JumpGame sol opt - leetcode

--- src/com/rsakin/practise/leetcode/JumpGame.java
+++ src/com/rsakin/practise/leetcode/JumpGame.java
@@ -8,7 +8,7 @@ public class JumpGame {
         int jumpSize = 0, curLast = 0, curLastest = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             curLastest = Math.max(curLastest, i + nums[i]);
-            if (i == curLast) {
+            if (i == curLast  || curLastest >= nums.length - 1) {
                 ++jumpSize;
                 curLast = curLastest;
                 if (curLast >= nums.length - 1) {
@@ -21,9 +21,9 @@ public class JumpGame {
 
     public static void main(String[] args) {
 
-        int[] paths = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
-        System.out.println("Solution for " + Arrays.toString(paths) + " : ");
-        System.out.println("Min step to end : " + new JumpGame().jump(paths));
+        int[] path = {10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
+        System.out.println("Path : " + Arrays.toString(path));
+        System.out.println("Min step to end : " + new JumpGame().jump(path));
 
     }
 
