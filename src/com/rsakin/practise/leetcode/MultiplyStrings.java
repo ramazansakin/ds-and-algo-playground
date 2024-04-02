53be948ff5a1d0affa59abadbe396aac144be2c1
ramazansakin <ramazansakin63@gmail.com>
2024-04-02 09:27:01 +0300

    feat : MultiplyStrings sol up - leetcode

--- src/com/rsakin/practise/leetcode/MultiplyStrings.java
+++ src/com/rsakin/practise/leetcode/MultiplyStrings.java
@@ -5,21 +5,6 @@ import java.util.stream.Collectors;
 
 public class MultiplyStrings {
 
-    public String multiply2(String num1, String num2) {
-        long no1 = strToInt(num1);
-        long no2 = strToInt(num2);
-        long sum = no1 * no2;
-        return "" + sum;
-    }
-
-    private long strToInt(String number) {
-        long convertedNum = 0;
-        for (int i = 0; i < number.length(); i++) {
-            convertedNum = 10 * convertedNum + (number.charAt(i) - '0');
-        }
-        return convertedNum;
-    }
-
     public String multiply(String num1, String num2) {
         if ("0".equals(num1) || "0".equals(num2)) return "0";
 
@@ -46,6 +31,26 @@ public class MultiplyStrings {
         String num1 = "6913259244", num2 = "71103343";
         System.out.println("Solution for " + num1 + " * " + num2 + " : " + new MultiplyStrings().multiply(num1, num2));
 
+        // case 2
+        String num3 = "123", num4 = "456";
+        System.out.println("Solution for " + num3 + " * " + num4 + " : " + new MultiplyStrings().multiply(num3, num4));
+
+    }
+
+    // Alternative easier way to solve
+    public String multiply2(String num1, String num2) {
+        long no1 = strToInt(num1);
+        long no2 = strToInt(num2);
+        long sum = no1 * no2;
+        return "" + sum;
+    }
+
+    private long strToInt(String number) {
+        long convertedNum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            convertedNum = 10 * convertedNum + (number.charAt(i) - '0');
+        }
+        return convertedNum;
     }
 
 }
