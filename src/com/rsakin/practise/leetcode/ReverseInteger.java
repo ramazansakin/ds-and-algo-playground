1a2cec70d7de806bd3a6ca5492178a830fa4c58e
ramazansakin <ramazansakin63@gmail.com>
2024-04-05 18:33:24 +0300

    feat : ReverseInteger sol up - leetcode

--- src/com/rsakin/practise/leetcode/ReverseInteger.java
+++ src/com/rsakin/practise/leetcode/ReverseInteger.java
@@ -10,4 +10,24 @@ public class ReverseInteger {
         }
         return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
     }
+
+    public static void main(String[] args) {
+        ReverseInteger test = new ReverseInteger();
+        test.runTests();
+    }
+
+    public void runTests() {
+        // Test cases
+        int[] testCases = {123, -123, 120, 0, 1534236469};
+
+        // Expected results
+        int[] expectedResults = {321, -321, 21, 0, 0};
+
+        // Run tests
+        for (int i = 0; i < testCases.length; i++) {
+            int result = reverse(testCases[i]);
+            System.out.println("Input: " + testCases[i] + ", Output: " + result + ", Expected: " + expectedResults[i] + ", Result: " + (result == expectedResults[i] ? "Pass" : "Fail"));
+        }
+    }
+
 }
