b3c91734da951bc284f075415ae326b050fe2932
ramazansakin <ramazansakin63@gmail.com>
2024-04-09 10:09:08 +0300

    feat : TreeAverageLevelSolution sol up with test cases - leetcode

--- src/com/rsakin/practise/leetcode/TreeAverageLevelSolution.java
+++ src/com/rsakin/practise/leetcode/TreeAverageLevelSolution.java
@@ -40,4 +40,28 @@ public class TreeAverageLevelSolution {
         return averages;
     }
 
+    public static void main(String[] args) {
+        TreeAverageLevelSolution main = new TreeAverageLevelSolution();
+
+        // Test Case 1
+        TreeNode root1 = new TreeNode(3);
+        root1.left = new TreeNode(9);
+        root1.right = new TreeNode(20);
+        root1.right.left = new TreeNode(15);
+        root1.right.right = new TreeNode(7);
+        List<Double> result1 = main.averageOfLevels(root1);
+        System.out.println("Average of levels for Test Case 1: " + result1);
+
+        // Test Case 2
+        TreeNode root2 = new TreeNode(3);
+        root2.left = new TreeNode(1);
+        root2.right = new TreeNode(5);
+        root2.left.left = new TreeNode(0);
+        root2.left.right = new TreeNode(2);
+        root2.right.left = new TreeNode(4);
+        root2.right.right = new TreeNode(6);
+        List<Double> result2 = main.averageOfLevels(root2);
+        System.out.println("Average of levels for Test Case 2: " + result2);
+    }
+
 }
