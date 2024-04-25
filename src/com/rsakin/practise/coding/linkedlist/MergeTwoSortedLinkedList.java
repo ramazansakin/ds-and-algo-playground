e02db085b3f0cdd064713aa252d3ea9ed746977f
ramazansakin <ramazansakin63@gmail.com>
2024-04-25 14:10:47 +0300

    feat : WordPa sol added - leetcode

--- src/com/rsakin/practise/coding/linkedlist/MergeTwoSortedLinkedList.java
+++ src/com/rsakin/practise/coding/linkedlist/MergeTwoSortedLinkedList.java
@@ -1,21 +1,13 @@
 package com.rsakin.practise.coding.linkedlist;
 
 
-// Definition for singly-linked list.
+// Definition for singly-linked list
 class ListNode {
     int val;
     ListNode next;
 
-    ListNode() {
-    }
-
     ListNode(int val) {
         this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
     }
 }
 
