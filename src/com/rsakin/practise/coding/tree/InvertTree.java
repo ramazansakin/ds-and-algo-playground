fb92e72f18e28b488e93dd839499030cd6101105
ramazansakin <ramazansakin63@gmail.com>
2024-04-30 14:13:22 +0300

    feat : InvertTree sol up - leetcode

--- src/com/rsakin/practise/coding/tree/InvertTree.java
+++ src/com/rsakin/practise/coding/tree/InvertTree.java
@@ -2,22 +2,33 @@ package com.rsakin.practise.coding.tree;
 
 public class InvertTree {
 
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null) {
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null)
             return null;
-        }
 
         // Swap the left and right subtrees recursively
         TreeNode left = invertTree(root.left);
         TreeNode right = invertTree(root.right);
-        root.left = right;
         root.right = left;
+        root.left = right;
 
         return root;
     }
 
     public static void main(String[] args) {
 
+        // Sample test case
+        TreeNode root = new TreeNode(20);
+        root.left = new TreeNode(10);
+        root.right = new TreeNode(30);
+        root.left.left = new TreeNode(7);
+        root.left.right = new TreeNode(15);
+        root.right.left = new TreeNode(25);
+        root.right.right = new TreeNode(40);
+
+        // Check if the tree is a BST
+        TreeNode invertedTree = InvertTree.invertTree(root);
+        System.out.println("Root value : " + invertedTree.val);
     }
 
 }
