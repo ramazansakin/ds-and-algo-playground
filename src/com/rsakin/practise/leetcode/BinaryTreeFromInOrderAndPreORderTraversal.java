ef057bd7c6f9b4cc1ec72c808dc3fe1bb7014055
ramazansakin <ramazansakin63@gmail.com>
2024-05-02 15:57:46 +0300

    feat : BinaryTreeFromInOrderAndPreORderTraversal sol added

---
+++ src/com/rsakin/practise/leetcode/BinaryTreeFromInOrderAndPreORderTraversal.java
@@ -0,0 +1,60 @@
+package com.rsakin.practise.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinaryTreeFromInOrderAndPreORderTraversal {
+
+    int preorderIndex;
+    Map<Integer, Integer> inorderIndexMap;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        preorderIndex = 0;
+        inorderIndexMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++){
+            inorderIndexMap.put(inorder[i], i);
+        }
+        return arrayToTree(preorder, 0, preorder.length - 1);
+    }
+
+    public TreeNode arrayToTree(int[] preorder, int left, int right){
+        if(left > right) return null;
+        int rootValue = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        root.left = arrayToTree(preorder, left, inorderIndexMap.get(rootValue) - 1);
+        root.right = arrayToTree(preorder, inorderIndexMap.get(rootValue) + 1, right);
+        return root;
+    }
+
+    // Function to print the binary tree (for testing)
+    public void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        printTree(root.left);
+        printTree(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeFromInOrderAndPreORderTraversal solution = new BinaryTreeFromInOrderAndPreORderTraversal();
+
+        // Example 1
+        int[] preorder1 = {3, 9, 20, 15, 7};
+        int[] inorder1 = {9, 3, 15, 20, 7};
+        TreeNode root1 = solution.buildTree(preorder1, inorder1);
+        System.out.println("Example 1:");
+        System.out.print("Output: ");
+        solution.printTree(root1);
+
+        // Example 2
+        int[] preorder2 = {-1};
+        int[] inorder2 = {-1};
+        TreeNode root2 = solution.buildTree(preorder2, inorder2);
+        System.out.println("\n\nExample 2:");
+        System.out.print("Output: ");
+        solution.printTree(root2);
+    }
+
+}
