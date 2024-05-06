327e21f349661c468be155969c7b32b03c013ade
ramazansakin <ramazansakin63@gmail.com>
2024-05-06 15:46:31 +0300

    chore : renamed MinJumpGame

--- src/com/rsakin/practise/leetcode/MinJumpGame.java
+++ src/com/rsakin/practise/leetcode/MinJumpGame.java
@@ -2,7 +2,7 @@ package com.rsakin.practise.leetcode;
 
 import java.util.Arrays;
 
-public class JumpGame {
+public class MinJumpGame {
 
     public int jump(int[] nums) {
         int jumpSize = 0, curLast = 0, curLastest = 0;
@@ -25,35 +25,35 @@ public class JumpGame {
         int[] nums1 = {2, 3, 1, 1, 4};
         System.out.println("Test Case 1");
         System.out.println("Path : " + Arrays.toString(nums1));
-        System.out.println("Min step to end : " + new JumpGame().jump(nums1));
+        System.out.println("Min step to end : " + new MinJumpGame().jump(nums1));
 
         System.out.println("------------------------");
 
         int[] nums2 = {2, 3, 0, 1, 4};
         System.out.println("Test Case 2");
         System.out.println("Path : " + Arrays.toString(nums2));
-        System.out.println("Min step to end : " + new JumpGame().jump(nums2));
+        System.out.println("Min step to end : " + new MinJumpGame().jump(nums2));
 
         System.out.println("------------------------");
 
         int[] nums3 = {3, 2, 1, 0, 4};
         System.out.println("Test Case 3");
         System.out.println("Path : " + Arrays.toString(nums3));
-        System.out.println("Min step to end : " + new JumpGame().jump(nums3));
+        System.out.println("Min step to end : " + new MinJumpGame().jump(nums3));
 
         System.out.println("------------------------");
 
         int[] nums4 = {1};
         System.out.println("Test Case 4");
         System.out.println("Path : " + Arrays.toString(nums4));
-        System.out.println("Min step to end : " + new JumpGame().jump(nums4));
+        System.out.println("Min step to end : " + new MinJumpGame().jump(nums4));
 
         System.out.println("------------------------");
 
         int[] path = {10, 10, 8, 7, 6, 5, 4, 3, 2, 1, 1, 0};
         System.out.println("Test Case 5");
         System.out.println("Path : " + Arrays.toString(path));
-        System.out.println("Min step to end : " + new JumpGame().jump(path));
+        System.out.println("Min step to end : " + new MinJumpGame().jump(path));
 
     }
 
