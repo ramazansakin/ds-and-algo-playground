cae996ef07994684edc1230f731f1d20be97a068
ramazansakin <ramazansakin63@gmail.com>
2024-05-14 07:42:27 +0300

    chore : ThreeSumClosest sol visit+

--- src/com/rsakin/practise/leetcode/ThreeSumClosest.java
+++ src/com/rsakin/practise/leetcode/ThreeSumClosest.java
@@ -11,13 +11,16 @@ public class ThreeSumClosest {
         for (int firstInd = 0; firstInd < nums.length - 2; firstInd++) {
             int secondInd = firstInd + 1;
             int thirdInd = nums.length - 1;
+
             while (secondInd < thirdInd) {
                 int total = nums[firstInd] + nums[secondInd] + nums[thirdInd];
                 int currDiff = Math.abs(target - total);
                 if (currDiff == 0)
                     return total;
+
                 if (currDiff < Math.abs(target - closestSum))
                     closestSum = total;
+
                 if (total < target) secondInd++;
                 else if (total > target) thirdInd--;
             }
