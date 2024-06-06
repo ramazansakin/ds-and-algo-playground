0f9b5ae4fefc8ce40bff27ed555e1a7f15ff3ef6
ramazansakin <ramazansakin63@gmail.com>
2024-06-06 08:20:00 +0300

    feat : RemoveDuplicates2 visit+

--- src/com/rsakin/practise/coding/array/RemoveDuplicates2.java
+++ src/com/rsakin/practise/coding/array/RemoveDuplicates2.java
@@ -21,7 +21,7 @@ public class RemoveDuplicates2 {
                 count = 1;
             }
 
-            // If the count is less than or equal to 2, keep the current element
+            // If the count is equal or less than 2, keep the current element
             if (count <= 2) {
                 // Place the current element at the next position where a unique element should be placed
                 nums[k++] = nums[i];
@@ -39,32 +39,36 @@ public class RemoveDuplicates2 {
         int result1 = solution.removeDuplicates(nums1);
         System.out.println("Test Case 1: " + result1); // Expected: 5
         System.out.print("Modified Array: ");
+
         for (int i = 0; i < result1; i++) {
             System.out.print(nums1[i] + " ");
         }
         System.out.println("------------------");
+
 
         // Test Case 2: Some Duplicates Exceeding Twice
         int[] nums2 = {1, 1, 1, 2, 2, 3, 3, 3, 3, 4};
         int result2 = solution.removeDuplicates(nums2);
         System.out.println("Test Case 2: " + result2); // Expected: 7
         System.out.print("Modified Array: ");
+
         for (int i = 0; i < result2; i++) {
             System.out.print(nums2[i] + " ");
         }
         System.out.println("------------------");
+
 
         // Test Case 3: All Elements Are the Same
         int[] nums3 = {2, 2, 2, 2, 2};
         int result3 = solution.removeDuplicates(nums3);
         System.out.println("Test Case 3: " + result3); // Expected: 2
         System.out.print("Modified Array: ");
+
         for (int i = 0; i < result3; i++) {
             System.out.print(nums3[i] + " ");
         }
         System.out.println();
 
     }
-
 
 }
